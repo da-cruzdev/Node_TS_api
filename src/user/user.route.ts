bb1b6511fec278ee85d4a75cdcd6ecf27d65148f
Da-cruz <wilfrieddacruz@arolitec.com>
2023-04-11 16:45:18 +0000

    refactoring user.route

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -5,16 +5,7 @@ import { signUp } from "./user.controller";
 const UserRoutes = (prisma: PrismaClient): Router => {
   const router = express.Router();
 
-  router.post("/user/create", async (req, res) => {
-    const { name, email, password } = req.body;
-
-    try {
-      const newUser = await signUp(name, email, password);
-      res.json(newUser);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to signup" });
-    }
-  });
+  router.post("/auth/create", signUp);
 
   router.get("/", (req, res) => {
     res.send("Hello");
