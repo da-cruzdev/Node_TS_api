012f5ead1d31e59849ab4f127066cd28c925c6b0
Da-cruz <wilfrieddacruz@arolitec.com>
2023-04-11 16:44:28 +0000

    add validation to user

---
+++ src/user/user.dto.ts
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+export const userSchema = Joi.object({
+  name: Joi.string().min(3).max(30).trim().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "fr"] } })
+    .trim()
+    .required(),
+  password: Joi.string()
+    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+    .trim()
+    .required(),
+});
+
+export interface CreateUserDTO {
+  name: string;
+  email: string;
+  password: string;
+}
