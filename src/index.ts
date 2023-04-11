b9c4dfe0603cff357502abacdc6c5b465f52d7c8
Da-cruz <wilfrieddacruz@arolitec.com>
2023-04-11 15:24:37 +0000

    create index.ts to run server

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import UserRoutes from "./user/user.route";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit();
+}
+
+const port: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", UserRoutes(prisma));
+
+app.listen(port, () => {
+  console.log(`Listennig on port ${port}`);
+});
