b80c958781141ddbe6fc1ee4d4ebba1f1e9f006e
Da-cruz <wilfrieddacruz@arolitec.com>
2023-04-17 17:24:34 +0000

    add method in transaction module

--- src/transactions/dto/transaction.dto.ts
+++ src/transactions/dto/transaction.dto.ts
@@ -1,6 +1,7 @@
 interface TransactionData {
   amount: number;
   accountIbanReceiver?: string;
+  currency: "USD" | "EURO";
   transactionType: "credit" | "debit" | "transfert";
   accountIbanEmitter?: string;
 }
--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -7,6 +7,22 @@ import TransactionData from "./dto/transaction.dto";
 
 const prisma = new PrismaClient();
 
+const convertCurrency = (amount: number, fromCurrency: string): number => {
+  const exchangeRates: Record<string, number> = {
+    EURO: 1,
+    USD: 1.2,
+    FCFA: 656,
+  };
+
+  if (!exchangeRates[fromCurrency]) {
+    throw new Error(`Currency ${fromCurrency} is not supported`);
+  }
+
+  const convertedAmount = amount / exchangeRates[fromCurrency];
+
+  return Math.round(convertedAmount * 100) / 100;
+};
+
 const validateTransactionData = (transactionData: TransactionData) => {
   const validation = TransactionSchema.validate(transactionData, {
     abortEarly: false,
@@ -62,32 +78,74 @@ const transferTransaction = async (transactionData: any) => {
   });
 };
 
+const getAccountBalance = async (accountIban: string): Promise<number> => {
+  try {
+    const account = await prisma.account.findUnique({
+      where: {
+        iban: accountIban,
+      },
+    });
+
+    if (!account) {
+      throw new Error("Account not found");
+    }
+
+    return account.balance;
+  } catch (error: any) {
+    throw new Error(`Failed to get account balance: ${error.message}`);
+  }
+};
+
 export const createTransaction = async (req: Request, res: Response) => {
   try {
-    const { amount, transactionType, accountIbanEmitter, accountIbanReceiver } =
-      req.body;
+    const {
+      amount,
+      transactionType,
+      currency,
+      accountIbanEmitter,
+      accountIbanReceiver,
+    } = req.body;
 
     let transactionData: TransactionData;
 
     switch (transactionType) {
       case "credit":
         transactionData = {
-          amount,
+          amount: convertCurrency(amount, currency),
           transactionType,
+          currency,
           accountIbanReceiver,
         };
         break;
       case "debit":
+        const accountEmitter = await prisma.account.findUnique({
+          where: { iban: accountIbanEmitter },
+        });
+        if (accountEmitter?.accountType === "blocked") {
+          throw new Error(
+            "Blocked accounts can only perform credit transactions"
+          );
+        }
         transactionData = {
-          amount,
+          amount: amount,
           transactionType,
+          currency: "EURO",
           accountIbanEmitter,
         };
         break;
       case "transfert":
+        const accountEmitterTransfer = await prisma.account.findUnique({
+          where: { iban: accountIbanEmitter },
+        });
+        if (accountEmitterTransfer?.accountType === "blocked") {
+          throw new Error(
+            "Blocked accounts can only perform credit transactions"
+          );
+        }
         transactionData = {
-          amount,
+          amount: convertCurrency(amount, currency),
           transactionType,
+          currency,
           accountIbanReceiver,
           accountIbanEmitter,
         };
@@ -105,11 +163,26 @@ export const createTransaction = async (req: Request, res: Response) => {
     switch (transactionType) {
       case "credit":
         await creditTransaction(transactionData);
+
         break;
       case "debit":
+        if (
+          transactionData.amount > (await getAccountBalance(accountIbanEmitter))
+        ) {
+          throw new Error(
+            "Insufficient balance in the account for the debit transaction"
+          );
+        }
         await debitTransaction(transactionData);
         break;
       case "transfert":
+        if (
+          transactionData.amount > (await getAccountBalance(accountIbanEmitter))
+        ) {
+          throw new Error(
+            "Insufficient balance in the account for the transfer transaction"
+          );
+        }
         await transferTransaction(transactionData);
         break;
       default:
@@ -117,25 +190,35 @@ export const createTransaction = async (req: Request, res: Response) => {
     }
 
     res.status(200).json(createdTransaction);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to create transaction" });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
   }
 };
 
-export const getAllTransactions = async (req: any, res: any) => {
-  try {
-    const transactions = await prisma.transaction.findMany();
-    const formattedTransaction = transactions.map((transaction) =>
-      formatDataResponse(transaction, "Transaction")
-    );
+export const getAllTransactions = async (req: Request, res: Response) => {
+  const { page, pageSize } = req.query;
+  const pageNumber = parseInt(page as string) || 1;
+  const pageSizeNumber = parseInt(pageSize as string) || 5;
 
-    const response = await getdataWithPagination(formattedTransaction, req);
-    res.status(200).json(response);
-  } catch (error) {
-    console.log(error);
+  const totalRecords = await prisma.transaction.count();
+  const totalPages = Math.ceil(totalRecords / pageSizeNumber);
+  const currentPage = pageNumber > totalPages ? totalPages : pageNumber;
 
-    res.status(500).json({ error: error });
-  }
+  const skip = Math.max((currentPage - 1) * pageSizeNumber, 0);
+
+  const transactions = await prisma.transaction.findMany({
+    skip: skip,
+    take: pageSizeNumber,
+  });
+
+  const response = {
+    totalRecords: totalRecords,
+    totalPages: totalPages,
+    currentPage: currentPage,
+    transactions: transactions,
+  };
+
+  res.status(200).json(response);
 };
 
 export const getOneTransaction = async (id: number) => {
